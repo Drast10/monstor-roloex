89cd8a316741152367a37cb534be856210231378
Drashti Gadhia <drashtibgadhia10@gmail.com>
2019-07-23 22:31:36 +0200

    Updates

---
+++ precache-manifest.1bf1b3f9b194efed38c23a5c9619a4a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4a815499422837293fe0cfc1ca4ec1",
+    "url": "/monstor-roloex/index.html"
+  },
+  {
+    "revision": "df76b4bce8c72c358b89",
+    "url": "/monstor-roloex/static/css/main.da16bd12.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monstor-roloex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "df76b4bce8c72c358b89",
+    "url": "/monstor-roloex/static/js/main.0910c488.chunk.js"
+  },
+  {
+    "revision": "db730ebdd620fb9d55d1",
+    "url": "/monstor-roloex/static/js/runtime~main.706d4404.js"
+  }
+]);
