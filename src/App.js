e328c45e6498157226670a7291b80d33f831866f
Drashti Gadhia <drashtibgadhia10@gmail.com>
2019-07-23 10:12:00 +0200

    fetch and display data on browser using api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/Card-list.component';
 
-function App() {
+class App extends React.Component {
+  state={
+    monsters:[]
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({monsters:users}))
+  }
+ render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <CardList><h1>Evee</h1></CardList> */}
+      <CardList monsters={this.state.monsters}/>
+    
+    
     </div>
   );
+ }
 }
 
 export default App;
